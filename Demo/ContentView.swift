0810023d68e725080f397d58622522c9108e7ca8
Aung Ko Min <jonahaung@gmail.com>
2021-12-21 19:20:50 +0800

    Initial Commit

---
+++ Demo/ContentView.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Aung Ko Min on 12/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selectedInstrument: Instrument = .guitar
+    @State private var selectedKey: String = "C"
+    @State private var selectedSuffix: String = "major"
+    
+    private let instruments = [Instrument.guitar, Instrument.ukulele]
+    private var foundChords: [Chord.Position] {
+        selectedInstrument.findChordPositions(key: selectedKey, suffix: selectedSuffix)
+    }
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                
+                Text("Fretboard")
+                    .font(.largeTitle)
+                    .padding()
+                
+                HStack {
+                    Picker("Instrument", selection: $selectedInstrument) {
+                        ForEach(instruments, id: \.self) {
+                            Text($0.name.capitalized(with: nil))
+                        }
+                    }
+                    
+                    Picker("Key", selection: $selectedKey) {
+                        ForEach(selectedInstrument.keys, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    
+                    Picker("Suffix", selection: $selectedSuffix) {
+                        ForEach(selectedInstrument.suffixes, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                HStack(spacing: 16) {
+                    ForEach(foundChords, id: \.self) { position in
+                        FretboardView(position: position)
+                            .frame(width: 100, height: 200)
+                    }
+                }
+                
+                Spacer()
+            }
+        }
+        .frame(width: 500, height: 400)
+        .padding()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
